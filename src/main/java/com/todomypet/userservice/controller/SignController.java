221cb1086947a7dfaf68d819aadffbbcdd4b010e
dawonseo <oloielo@naver.com>
2023-07-20 15:29:22 +0900

    feat: 회원가입 유효성 검사 추가

--- src/main/java/com/todomypet/userservice/controller/SignController.java
+++ src/main/java/com/todomypet/userservice/controller/SignController.java
@@ -2,7 +2,9 @@ package com.todomypet.userservice.controller;
 
 import com.todomypet.userservice.dto.*;
 import com.todomypet.userservice.service.SignService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,7 +16,7 @@ public class SignController {
     private final SignService signService;
 
     @PostMapping("/users/sign-up")
-    public SuccessResDTO<SignUpResDTO> signUp(@RequestPart(value = "signUpInfo") SignUpReqDTO signUpInfo,
+    public SuccessResDTO<SignUpResDTO> signUp(@Valid @RequestPart(value = "signUpInfo") SignUpReqDTO signUpInfo,
                                               @RequestPart(value = "profilePic", required = false) MultipartFile multipartFile) {
         SignUpResDTO response = SignUpResDTO.builder().id(signService.signUp(signUpInfo, multipartFile)).build();
         return new SuccessResDTO<>(response);
--- src/main/java/com/todomypet/userservice/dto/SignUpReqDTO.java
+++ src/main/java/com/todomypet/userservice/dto/SignUpReqDTO.java
@@ -1,8 +1,6 @@
 package com.todomypet.userservice.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,8 +11,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpReqDTO {
+    @NotBlank
     private String email;
+
+    @Min(value = 8) @Max(value = 20)
     private String password;
+
+    @Min(value = 1) @Max(value = 15)
     private String nickname;
+
+    @Max(value = 80)
     private String bio;
 }
