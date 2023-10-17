0b9c778a94d911eabca1f4422fdbc8abec269e6d
dawonseo <oloielo@naver.com>
2023-10-17 17:52:06 +0900

    chore: pathvariable로 수정

--- src/main/java/com/todomypet/userservice/controller/SignController.java
+++ src/main/java/com/todomypet/userservice/controller/SignController.java
@@ -36,17 +36,17 @@ public class SignController {
 
     @Operation(summary = "이메일 중복 체크",
             description = "입력된 이메일을 사용하는 회원이 이미 존재하면 false, 아니면 true를 return합니다.")
-    @GetMapping("/duplication-check")
-    public SuccessResDTO<DuplicationCheckResDTO> emailDuplicationCheck(@RequestBody DuplicationCheckReqDTO duplicationCheckReqDTO) {
+    @GetMapping("/duplication-check/{checkedEmail}")
+    public SuccessResDTO<DuplicationCheckResDTO> emailDuplicationCheck(@PathVariable String checkedEmail) {
         DuplicationCheckResDTO response = DuplicationCheckResDTO.builder()
-                .response(signService.duplicationCheck(duplicationCheckReqDTO.getCheckedEmail())).build();
+                .response(signService.duplicationCheck(checkedEmail)).build();
         return new SuccessResDTO<>(response);
     }
 
     @Operation(summary = "확인 메일 발송", description = "랜덤 코드를 생성하여 확인 메일을 발송합니다.")
-    @GetMapping("/send-check-email")
-    public SuccessResDTO<SendCheckEmailResDTO> sendCheckEmail(@RequestBody SendCheckEmailReqDTO request) throws Exception {
-        String code = signService.sendCheckEmail(request.getReceiveEmail());
+    @GetMapping("/send-check-email/{receiveEmail}")
+    public SuccessResDTO<SendCheckEmailResDTO> sendCheckEmail(@PathVariable String receiveEmail) throws Exception {
+        String code = signService.sendCheckEmail(receiveEmail);
         SendCheckEmailResDTO response = SendCheckEmailResDTO.builder().checkCode(code).build();
         return new SuccessResDTO<>(response);
     }
