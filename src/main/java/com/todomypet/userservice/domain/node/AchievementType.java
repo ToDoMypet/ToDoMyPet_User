bafdda3f02aed459b4055ee1002c76ea50decab8
dawonseo <oloielo@naver.com>
2023-11-13 16:25:44 +0900

    chore: 업적 타입 추가

--- src/main/java/com/todomypet/userservice/domain/node/AchievementType.java
+++ src/main/java/com/todomypet/userservice/domain/node/AchievementType.java
@@ -4,7 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum AchievementType {
-    ATTENDANCE("출석")
+    ATTENDANCE("출석"),
+    ACHIEVE("달성"),
+    EVOLUTION("진화"),
+    GRADUATION("최종"),
+    ACQUIREMENT("획득")
     ;
 
     private final String achievementType;
