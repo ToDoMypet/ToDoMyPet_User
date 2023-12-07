c08776c6080201ccfc211d440d33557c4ffad310
dawonseo <oloielo@naver.com>
2023-12-07 16:09:33 +0900

    fix: achievement req dto 수정

--- src/main/java/com/todomypet/userservice/dto/achievement/AchieveReqDTO.java
+++ src/main/java/com/todomypet/userservice/dto/achievement/AchieveReqDTO.java
@@ -1,10 +1,14 @@
 package com.todomypet.userservice.dto.achievement;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AchieveReqDTO {
     private String achievementId;
 }
