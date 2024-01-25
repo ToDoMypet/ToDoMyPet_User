9c8d7b1e5c23c007f3128bdda629d2204f3152de
dawonseo <oloielo@naver.com>
2024-01-25 13:35:28 +0900

    chore: swagger 문서 타이틀 수정

--- src/main/java/com/todomypet/userservice/service/MailService.java
+++ src/main/java/com/todomypet/userservice/service/MailService.java
@@ -8,6 +8,7 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -45,153 +46,139 @@ public class MailService {
     public String sendMail(String to) throws Exception {
         eCode = createCode();
         MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-        message.addRecipients(Message.RecipientType.TO, to);
-        message.setSubject("[To-do My Pet] 이메일 인증 코드 발송");
+        // 이메일 주소 및 제목 설정
+        helper.setTo(to);
+        helper.setSubject("[To-do My Pet] 이메일 인증 코드 발송");
 
-        String msg = """
+        String htmlContent = """
                 <!DOCTYPE html>
-                <html lang="en">
-                    <head>
-                        <style>
-                            body {
-                                margin: 0;
-                                background-color: #ebebeb;
-                            }
+                        <html lang="en">
+                            <head>
+                                <style>
+                                    body {
+                                        margin: 0;
+                                        background-color: #ebebeb;
+                                    }
+                                       
+                                    table.container {
+                                        width: 100%;
+                                        height: 100vh;
+                                        margin: 0;
+                                        padding: 0;
+                                        border-collapse: collapse;
+                                    }
+                                       
+                                    td.email-container {
+                                        vertical-align: middle;
+                                        text-align: center;
+                                        position: relative;
+                                    }
+                                       
+                                    .logo {
+                                        position: absolute;
+                                        top: 7%;
+                                        left: 50%;
+                                        transform: translateX(-50%);
+                                        max-width: 100%;
+                                        height: auto;
+                                        z-index: 1;
+                                    }
+                                       
+                                    table.content-container {
+                                        background-color: white;
+                                        height: 600px;
+                                        width: 600px;
+                                        border-radius: 10px;
+                                        position: relative;
+                                        margin: 0 auto;
+                                        padding-top: 5%;
+                                        border-collapse: collapse;
+                                    }
+                                       
+                                    td.code-box {
+                                        background-color: #f9f9f9;
+                                        width: 260px;
+                                        height: 64px;
+                                        margin: auto;
+                                        text-align: center;
+                                        border-radius: 10px;
+                                        display: flex;
+                                        justify-content: center;
+                                        align-items: center;
+                                        margin-top: 17%;
+                                        margin-bottom: 10%;
+                                    }
+                                       
+                                    .footer {
+                                        background-color: #f9f9f9;
+                                        width: 100%;
+                                        height: 145px;
+                                        position: absolute;
+                                        bottom: 0;
+                                        border-radius: 0 0 10px 10px;
+                                    }
+                                </style>
+                            </head>
+                                       
+                            <body>
+                                <table class="container" style="width:100%; height:100vh; margin:0; padding:0; border-collapse:collapse;">
+                                            <thead>
+                                                <tr>
+                                                    <th colspan="2">이메일 인증 코드 발송</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td colspan="2">
+                                                        <a>안녕하세요 To Do My Pet 입니다!</a>
+                                                        <br />
+                                                        <br />
+                                                        <a>이메일 주소 인증을 요청하였습니다.</a>
+                                                        <br />
+                                                        <a>인증 코드 입력란에 아래 6자리 코드를 입력해 주세요!</a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="code-box" colspan="2">
+                                                        <h2>""";
                                 
-                            table.container {
-                                width: 100%;
-                                height: 100vh;
-                                margin: 0;
-                                padding: 0;
-                                border-collapse: collapse;
-                            }
-                                
-                            td.email-container {
-                                vertical-align: middle;
-                                text-align: center;
-                                position: relative;
-                            }
-                                
-                            .logo {
-                                position: absolute;
-                                top: 7%;
-                                left: 50%;
-                                transform: translateX(-50%);
-                                max-width: 100%;
-                                height: auto;
-                                z-index: 1;
-                            }
-                                
-                            table.content-container {
-                                background-color: white;
-                                height: 600px;
-                                width: 600px;
-                                border-radius: 10px;
-                                position: relative;
-                                margin: 0 auto;
-                                padding-top: 5%;
-                                border-collapse: collapse;
-                            }
-                                
-                            td.code-box {
-                                background-color: #f9f9f9;
-                                width: 260px;
-                                height: 64px;
-                                margin: auto;
-                                text-align: center;
-                                border-radius: 10px;
-                                display: flex;
-                                justify-content: center;
-                                align-items: center;
-                                margin-top: 17%;
-                                margin-bottom: 10%;
-                            }
-                                
-                            .footer {
-                                background-color: #f9f9f9;
-                                width: 100%;
-                                height: 145px;
-                                position: absolute;
-                                bottom: 0;
-                                border-radius: 0 0 10px 10px;
-                            }
-                        </style>
-                    </head>
-                                
-                    <body>
-                        <table class="container">
-                            <tr>
-                                <td class="email-container">
-                                    <img
-                                        src="https://todomypet.s3.ap-northeast-2.amazonaws.com/emaillImage.png"
-                                        class="logo"
-                                        height="110"
-                                        width="130"
-                                    />
-                                
-                                    <table class="content-container">
-                                        <tr>
-                                            <td>
-                                                <h2>이메일 인증 코드 발송</h2>
-                                                <br />
-                                                <br />
-                                                <a>안녕하세요 To Do My Pet 입니다!</a>
-                                                <br />
-                                                <br />
-                                                <a>이메일 주소 인증을 요청하였습니다.</a>
-                                                <br />
-                                                <a>인증 코드 입력란에 아래 6자리 코드를 입력해 주세요!</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="code-box">
-                                                <h2>
-                    """;
-        msg += eCode;
-        msg += """
-                </h2>
-                                                   </td>
-                                               </tr>
-                                               <tr>
-                                                   <td class="footer">
-                                                       <br />
-                                                       <br />
-                                                       <a style="color: #a1a1a1">본 메일은 이메일 인증을 위한 발신전용 메일입니다. </a>
-                                                       <br />
-                                                       <a style="color: #a1a1a1"
-                                                           >본 메일과 관련되어 궁금하신 점이나 불편하신 사항은 고객센터에 문의해 주시기
-                                                           바랍니다.</a
-                                                       >
-                                                       <br />
-                                                       <br />
-                                                       <a style="color: #a1a1a1">ⓒTO DO MY PET</a>
-                                                   </td>
-                                               </tr>
-                                           </table>
-                                       </td>
-                                   </tr>
-                               </table>
-                           </body>
-                       </html>
-                """;
-//        msg += "<div style='margin:100px;'>";
-//        msg += "<h1> To-do My Pet 이메일 인증 코드 발송 </h1>";
-//        msg += "<hr>";
-//        msg += "<br>";
-//        msg += "안녕하십니까? To-do My Pet입니다.";
-//        msg += "<br>";
-//        msg += "회원가입 페이지의 인증 코드 입력란에 아래 6자리 코드를 입력해 주세요.";
-//        msg += "<br>";
-//        msg += "<h3>";
-//        msg += eCode;
-//        msg += "</h3>";
-//        msg += "</div>";
+                        htmlContent += eCode;
+                        htmlContent += """
+                                            </h2>
+                                                                                            </td>
+                                                                                        </tr>
+                                                                                    </tbody>
+                                                                                    <tfoot>
+                                                                                        <tr>
+                                                                                            <td class="footer" colspan="2">
+                                                                                                <a style="color: #a1a1a1">본 메일은 이메일 인증을 위한 발신전용 메일입니다. </a>
+                                                                                                <br />
+                                                                                                <a style="color: #a1a1a1"
+                                                                                                    >본 메일과 관련되어 궁금하신 점이나 불편하신 사항은 고객센터에 문의해 주시기
+                                                                                                    바랍니다.</a
+                                                                                                >
+                                                                                                <br />
+                                                                                                <br />
+                                                                                                <a style="color: #a1a1a1">ⓒTO DO MY PET</a>
+                                                                                            </td>
+                                                                                        </tr>
+                                                                                    </tfoot>
+                                                                                </table>
+                                                                            </td>
+                                                                        </tr>
+                                                                    </tbody>
+                                                                </table>
+                                                            </body>
+                                                        </html> 
+                                """;
 
-        message.setText(msg, "utf-8", "html");
+        // HTML 내용 및 포맷 설정
+        helper.setText(htmlContent, true);
 
-        message.setFrom(new InternetAddress("todomypet@gmail.com", "To-do My Pet"));
-
+        // 보내는 사람 설정
+        helper.setFrom(new InternetAddress("todomypet@gmail.com", "To-do My Pet"));
         try {
             javaMailSender.send(message);
         } catch (MailException e) {
