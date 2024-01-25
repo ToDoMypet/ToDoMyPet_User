0f323ca927902e08bbe880ac8e1d67dfb16ccdfc
dawonseo <oloielo@naver.com>
2024-01-25 14:27:12 +0900

    fix: 메인 페이지 조회 시 서버간 통신 에러 수정

--- src/main/java/com/todomypet/userservice/service/PetServiceClient.java
+++ src/main/java/com/todomypet/userservice/service/PetServiceClient.java
@@ -5,14 +5,11 @@ import com.todomypet.userservice.dto.adopt.UpdateExperiencePointReqDTO;
 import com.todomypet.userservice.dto.adopt.UpdateExperiencePointResDTO;
 import com.todomypet.userservice.dto.pet.GetMainPetInfosResDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name="pet-service", url="${feign.pet.url}")
 public interface PetServiceClient {
-    @PostMapping(value = "/get-main-pet-infos", consumes = "application/json")
+    @GetMapping(value = "/get-main-pet-infos", consumes = "application/json")
     FeignClientResDTO<GetMainPetInfosResDTO> getMainPetInfosByUserId(@RequestHeader String userId);
 
     @PutMapping(value = "update-exp", consumes = "application/json")
