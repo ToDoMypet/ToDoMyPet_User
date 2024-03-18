0430679bf9ae7722dd74ede7a5d452d639162e83
dawonseo <oloielo@naver.com>
2024-03-18 14:31:25 +0900

    fix: 이메일 html 형식 수정

--- src/main/java/com/todomypet/userservice/service/MailService.java
+++ src/main/java/com/todomypet/userservice/service/MailService.java
@@ -53,126 +53,88 @@ public class MailService {
         helper.setSubject("[To-do My Pet] 이메일 인증 코드 발송");
 
         String htmlContent = """
-                <!DOCTYPE html>
-                        <html lang="en">
-                            <head>
-                                <style>
-                                    body {
-                                        margin: 0;
-                                        background-color: #ebebeb;
-                                    }
-                                       
-                                    table.container {
-                                        width: 100%;
-                                        height: 100vh;
-                                        margin: 0;
-                                        padding: 0;
-                                        border-collapse: collapse;
-                                    }
-                                       
-                                    td.email-container {
-                                        vertical-align: middle;
-                                        text-align: center;
-                                        position: relative;
-                                    }
-                                       
-                                    .logo {
-                                        position: absolute;
-                                        top: 7%;
-                                        left: 50%;
-                                        transform: translateX(-50%);
-                                        max-width: 100%;
-                                        height: auto;
-                                        z-index: 1;
-                                    }
-                                       
-                                    table.content-container {
+                <html lang="en">
+                    <body style="margin: 0; background-color: white">
+                        <div style="display: flex; justify-content: center; align-items: center; height: 100vh">
+                            <div style="text-align: center; position: relative">
+                                <img
+                                    src="https://todomypet.s3.ap-northeast-2.amazonaws.com/emaillImage.png"
+                                    height="110"
+                                    width="130"
+                                    style="position: absolute; top: -15%; left: 50%; transform: translateX(-50%); z-index: 1"
+                                />
+                                
+                                <div
+                                    style="
                                         background-color: white;
                                         height: 600px;
                                         width: 600px;
                                         border-radius: 10px;
-                                        position: relative;
-                                        margin: 0 auto;
+                                        z-index: 0;
                                         padding-top: 5%;
-                                        border-collapse: collapse;
-                                    }
-                                       
-                                    td.code-box {
-                                        background-color: #f9f9f9;
-                                        width: 260px;
-                                        height: 64px;
-                                        margin: auto;
-                                        text-align: center;
-                                        border-radius: 10px;
-                                        display: flex;
-                                        justify-content: center;
-                                        align-items: center;
-                                        margin-top: 17%;
-                                        margin-bottom: 10%;
-                                    }
-                                       
-                                    .footer {
-                                        background-color: #f9f9f9;
-                                        width: 100%;
-                                        height: 145px;
-                                        position: absolute;
-                                        bottom: 0;
-                                        border-radius: 0 0 10px 10px;
-                                    }
-                                </style>
-                            </head>
-                                       
-                            <body>
-                                <table class="container" style="width:100%; height:100vh; margin:0; padding:0; border-collapse:collapse;">
-                                            <thead>
-                                                <tr>
-                                                    <th colspan="2">이메일 인증 코드 발송</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td colspan="2">
-                                                        <a>안녕하세요 To Do My Pet 입니다!</a>
-                                                        <br />
-                                                        <br />
-                                                        <a>이메일 주소 인증을 요청하였습니다.</a>
-                                                        <br />
-                                                        <a>인증 코드 입력란에 아래 6자리 코드를 입력해 주세요!</a>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="code-box" colspan="2">
-                                                        <h2>""";
+                                    "
+                                >
+                                    <h2>이메일 인증 코드 발송</h2>
+                                    <br />
+                                    <br />
+                                    <a>안녕하세요 To Do My Pet 입니다!</a>
+                                    <br />
+                                    <br />
+                                    <a>이메일 주소 인증을 요청하였습니다.</a>
+                                    <br />
+                                    <a>인증 코드 입력란에 아래 6자리 코드를 입력해 주세요!</a>
+                                    <div
+                                        style="
+                                            background-color: #f9f9f9;
+                                            width: 260px;
+                                            height: 64px;
+                                            left: 260px;
+                                            margin: auto;
+                                            text-align: center;
+                                            border-radius: 10px;
+                                            display: flex;
+                                            justify-content: center;
+                                            align-items: center;
+                                            margin-top: 17%;
+                                            margin-bottom: 10%;
+                                        "
+                                    >
+                                        <h2>Bwq455</h2>
+                                    </div>
                                 
-                        htmlContent += eCode;
-                        htmlContent += """
-                                            </h2>
-                                                                                            </td>
-                                                                                        </tr>
-                                                                                    </tbody>
-                                                                                    <tfoot>
-                                                                                        <tr>
-                                                                                            <td class="footer" colspan="2">
-                                                                                                <a style="color: #a1a1a1">본 메일은 이메일 인증을 위한 발신전용 메일입니다. </a>
-                                                                                                <br />
-                                                                                                <a style="color: #a1a1a1"
-                                                                                                    >본 메일과 관련되어 궁금하신 점이나 불편하신 사항은 고객센터에 문의해 주시기
-                                                                                                    바랍니다.</a
-                                                                                                >
-                                                                                                <br />
-                                                                                                <br />
-                                                                                                <a style="color: #a1a1a1">ⓒTO DO MY PET</a>
-                                                                                            </td>
-                                                                                        </tr>
-                                                                                    </tfoot>
-                                                                                </table>
-                                                                            </td>
-                                                                        </tr>
-                                                                    </tbody>
-                                                                </table>
-                                                            </body>
-                                                        </html> 
-                                """;
+                                    <div
+                                        style="
+                                            background-color: #f9f9f9;
+                                            width: 100%;
+                                            height: 145px;
+                                            margin: auto;
+                                            text-align: center;
+                                
+                                            border-radius: 0 0 10px;
+                                
+                                            position: absolute;
+                                            bottom: 0;
+                                        "
+                                    >
+                                        <br />
+                                        <br />
+                                        <a style="color: #a1a1a1">본 메일은 이메일 인증을 위한 발신전용 메일입니다. </a>
+                                        <br />
+                                        <a style="color: #a1a1a1"
+                                            >본 메일과 관련되어 궁금하신 점이나 불편하신 사항은 고객센터에 문의해 주시기 바랍니다.
+                                        </a>
+                                
+                                        <br />
+                                        <br />
+                                
+                                        <a style="color: #a1a1a1"> ⓒTO DO MY PET </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </body>
+                </html>
+                """;
 
         // HTML 내용 및 포맷 설정
         helper.setText(htmlContent, true);
